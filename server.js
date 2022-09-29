5b48e08754fb8b4fa52b0bf42396cc694ad17a1d
Euommy <104454973+Euommy@users.noreply.github.com>
2022-09-29 19:39:21 -0400

    Added Express, and third-party modules

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-// import third-party nodejs module Connect
-const connect = require('connect');
+// Third party Modules
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from 'morgan';
+import session from "express-session";
+
+//Es Modules fix for__dirname
+import path, {dirname} from 'path';
+import { fileURLToPath } from'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // instantiate app-server
-const app = connect();
+const app = express();
 
 // custom middleware
 function helloWorld(req, res, next){
